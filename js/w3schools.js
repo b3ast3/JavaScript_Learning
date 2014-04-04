6b353e335d93a7db7a62130d48673597aa7dc7d0
b3ast3 <pradeepchowdary770@gmail.com>
2014-04-05 02:08:07 +0530

    Update w3schools.js

--- js/w3schools.js
+++ js/w3schools.js
@@ -1,4 +1,10 @@
 function myFunction()
 {
-document.getElementById("demo").innerHTML = Date();
+  document.getElementById("demo").innerHTML = Date();
+}
+
+function myFunction1()
+{
+  x = document.getElementById("demo"); //Find the element with id="demo"
+  x.innerHTML = "Hello JavaScript!";   //Change the HTML content
 }
